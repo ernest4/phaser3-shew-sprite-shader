123f3bc5eced676c94d1794457996a11f088e555
Ernestas Monkevicius <35740142+ernest4@users.noreply.github.com>
2021-12-01 07:42:04 +0000

    clean up

--- SkewQuad.ts
+++ SkewQuad.ts
@@ -47,22 +47,3 @@ class SkewQuad extends Phaser.Renderer.WebGL.Pipelines.SinglePipeline {
 }
 
 export default SkewQuad;
-
-// class GrayscalePipeline extends Phaser.Renderer.WebGL.Pipelines.SinglePipeline {
-//   constructor(game: Phaser.Game) {
-//     let config = {
-//       game,
-//       renderer: game.renderer,
-//       fragShader: `
-//       precision mediump float;
-//       uniform sampler2D uMainSampler;
-//       varying vec2 outTexCoord;
-//       void main(void) {
-//       vec4 color = texture2D(uMainSampler, outTexCoord);
-//       float gray = dot(color.rgb, vec3(0.299, 0.587, 0.114));
-//       gl_FragColor = vec4(vec3(gray), 1.0);
-//       }`,
-//     };
-//     super(config);
-//   }
-// }
